5dc37cdc7a79666048a8b66535f2f6728852a543
hmscop2022 <hmscop2022@gmail.com>
2022-09-21 23:36:19 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    얼굴인식APP
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
