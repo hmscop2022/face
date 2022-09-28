dd6bffb9161313858acb9af1f8984a77609ebcc8
hmscop2022 <hmscop2022@gmail.com>
2022-09-28 23:38:41 +0900

    헤더, 네비 html, css 초기 작업

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <div id="app">
-    얼굴인식APP
+  <div id="app" class="ly_cont">
+    <div class="ly_contInner">
+      <Header></Header>
+      <Navigation></Navigation>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/common/Header.vue'
+import Navigation from './components/common/Navigation.vue'
+
 export default {
   name: 'app',
   data () {
     return {
 
     }
+  },
+  components: {
+    'Header': Header,
+    'Navigation':Navigation,
   }
 }
 </script>
---
+++ src/components/common/Navigation.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <nav class="bl_tabNav">
+    <ul class="bl_tabNav_inner">
+      <li><span class="bl_tabNav_link is_active ic_image">업로드</span></li>
+      <li><a class="bl_tabNav_link ic_fileLines" href="#">분석결과</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.ic_image::before {
+  content:"\f03e";
+  font: var(--fa-font-solid);
+  font-size : 1rem;
+  margin-right: 5px;
+}
+.ic_fileLines::before {
+  content:"\f15c";
+  font: var(--fa-font-solid);
+  font-size : 1rem;
+  margin-right: 5px;
+}
+
+</style>
