b58974554938a61ae42f4c4fb28062aa8407282e
hmscop2022 <hmscop2022@gmail.com>
2022-10-01 10:16:23 +0900

    업로드 영역 버튼 레이아웃 조정

--- src/components/upload/Upload.vue
+++ src/components/upload/Upload.vue
@@ -2,12 +2,12 @@
   <div class="bl_upload">
     <h3 class="bl_upload_title">얼굴 사진과 닮은 꼴 유명인을 찾아드립니다</h3>
     <h4 class="bl_upload_subTitle">용량 2MB 미만의 이미지를 업로드 하세요!</h4>
-    <div class="bl_upload_imgWrapper">
+    <div class="bl_upload_imgBtn">
       <img class="bl_upload_img" src="https://placeimg.com/250/300/any/grayscale" width="250" height="300" alt="">
-    </div>
-    <div class="ly_btnTwoColumn">
-      <a class="el_btn ic_image" href="#">업로드</a>
-      <a class="el_btn ic_search" href="#">찾기</a>
+      <div class="ly_btnTwoColumn">
+        <a class="el_btn ic_image" href="#">업로드</a>
+        <a class="el_btn ic_search" href="#">찾기</a>.
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +23,7 @@ export default {
   margin: 0 auto;
   border: 2px solid #ddd;
   margin-top: 20px;
-  padding: 20px 60px;
+  padding: 20px 10px;
   text-align: center;
   border-radius: 8px;
 }
@@ -37,21 +37,28 @@ export default {
   color: #5d5d5d;
   margin-bottom: 10px;
 }
-.bl_upload_imgWrapper{
-
+.bl_upload_imgBtn{
+  display:inline-block;
+  width: 250px;
 }
 .bl_upload_img{
   display:inline-block;
 }
 .ly_btnTwoColumn{
-  width: 100%;
-  padding: 10px 24px;
+  display: inline-block;
+  box-sizing: border-box;
+  margin-top: 5px;
+  width: 250px;
+  height: 3em;
+  text-align: center;
 }
 .ly_btnTwoColumn .el_btn{
-  font-size: .9rem;
+  box-sizing: border-box;
+  display: inline-block;
+  width: 49%;
+  padding: 1em 0.3em;
+  font-size: .8rem;
   font-weight: bold;
 }
-.ly_btnTwoColumn .el_btn:first {
-  transform:translateX(-100);
-}
+
 </style>
